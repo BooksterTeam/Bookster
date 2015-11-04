b094a7dddeb7ac76364b910227333bef396503fc
nixoxo <nico.axtmann95@gmail.com>
2015-11-04 21:49:49 +0100

    added book property in author.js

--- src/main/webapp/scripts/app/entities/author/author.js
+++ src/main/webapp/scripts/app/entities/author/author.js
@@ -63,6 +63,7 @@ angular.module('booksterApp')
                                 return {
                                     forename: null,
                                     surname: null,
+                                    book:null,
                                     id: null
                                 };
                             }
