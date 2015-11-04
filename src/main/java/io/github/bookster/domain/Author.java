3dd051353d5c2ab2b2a982f2471c5264995c8301
nixoxo <nico.axtmann95@gmail.com>
2015-11-04 21:29:44 +0100

    added author

---
+++ src/main/java/io/github/bookster/domain/Author.java
@@ -0,0 +1,79 @@
+package io.github.bookster.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Author.
+ */
+
+@Document(collection = "author")
+public class Author implements Serializable {
+
+    @Id
+    private String id;
+
+    @Field("forename")
+    private String forename;
+
+    @Field("surname")
+    private String surname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Author author = (Author) o;
+
+        if ( ! Objects.equals(id, author.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+            "id=" + id +
+            ", forename='" + forename + "'" +
+            ", surname='" + surname + "'" +
+            '}';
+    }
+}
