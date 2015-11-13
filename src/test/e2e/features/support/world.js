9a90abc9de4481529eaefc7c0fe5fa2ba3a58b3a
dan-brown <daniel.brown@live.de>
2015-11-13 11:26:29 +0100

    actually starting testserver and running first dummy tests in protractor

---
+++ src/test/e2e/features/support/world.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+
+    this.World = function World(callback) {
+        this.prop = "Hello from the World!";
+
+        this.greetings = function (name, callback) {
+            console.log("\n----Hello " + name);
+            callback();
+        };
+
+        callback();
+    }
+};
