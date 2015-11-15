75661aef5b419082c1d9ed769b2b1045453702a4
nixoxo <nico.axtmann95@gmail.com>
2015-11-15 17:35:09 +0100

    database setup for books

--- src/main/webapp/scripts/app/market/market.controller.js
+++ src/main/webapp/scripts/app/market/market.controller.js
@@ -1,14 +1,17 @@
 angular.module('booksterApp')
-    .controller('MarketController', function ($scope) {
-        $scope.books = [
-            {
-                title: "yolo"
-            },
-            {
-                title: "yol1"
-            },
-            {
-                title: "yol2"
-            }
-        ]
+    .controller('MarketController', function ($scope, Book, ParseLinks) {
+        $scope.books = [];
+        $scope.loadAll = function() {
+            Book.query({page: $scope.page, size: 20}, function(result, headers) {
+                $scope.links = ParseLinks.parse(headers('link'));
+                $scope.books = result;
+            });
+        };
+
+        $scope.loadPage = function(page) {
+            $scope.page = page;
+            $scope.loadAll();
+        };
+
+        $scope.loadAll();
     });
